e91d4aec8060519f4a6d22b63b0d039727fa59e5
vanfigo <rodrigo.loyola@live.com>
2019-01-16 13:43:08 -0600

    last stable commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'topVoted', component: LandingComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
